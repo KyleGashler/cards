e9651b9a490d121265615afb4f66b2b661d4dafe
Kyle Gashler <10814854@uvu.edu>
2019-02-04 17:51:43 -0700

    draw 2 with files

---
+++ drawtwo.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+require 'vendor/autoload.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>21</title>
+</head>
+<body>
+    <div id="x">Draw 2 Cards</div>
+    <button id="draw" name="draw">Draw</button>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>21</title>
+</head>
+<body>
+    <div id="x">Draw 2 Cards</div>
+    <form action=""></form>
+    <button class="submit" id="draw" name="draw" onclick="<?php header("Location: drawtwo.php"); ?>">Draw</button>
+    
+</body>
+</html>
